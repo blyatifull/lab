22a2737d26aab32f45bcd01e50196e1473a226d4
blyatifull <ilya.lebedev.2003@list.ru>
2022-02-08 20:35:45 +0300

    append push / pop functions

--- libs/data_structures/vector_void/vector_void.c
+++ libs/data_structures/vector_void/vector_void.c
@@ -23,20 +23,69 @@ void reserveV(vectorVoid *v, size_t newCapacity){
         exit(1);
     }
 
-    (v)->data = (void *)realloc((v)->data, newCapacity * v->baseTypeSize);
+    (v)->data = (void *)realloc((v)->data, newCapacity * (v)->baseTypeSize);
     (v)->capacity = newCapacity;
 }
 
 void shrinkToFitV(vectorVoid *v){
-    reserveV(v, 0);
+    reserveV((v), 0);
 }
 
 void clearV(vectorVoid *v){
-    v->size = 0;
+    (v)->size = 0;
 }
 
 void deleteVector(vectorVoid  *v){
-    free(v->data);
+    free((v)->data);
+}
+
+bool isFullV(vectorVoid *v){
+    return (v)->size == (v)->capacity;
+}
+
+bool isEmpty(vectorVoid *v){
+    return (v)->size == 0;
+}
+
+void getVectorValueV(vectorVoid *v, size_t index, void *destination) {
+    if (index < (v)->size) {
+        char *source = (char *) (v)->data + index * (v)->baseTypeSize;
+        memcpy(destination, source, (v)->baseTypeSize);
+        return;
+    }
+
+    fprintf(stderr, "indexError: index[%u] is not exist", index);
+    exit(1);
+}
+
+void setVectorValueV(vectorVoid *v, size_t index, void *source) {
+    if (index < (v)->capacity) {
+        char *destination = (char *) (v)->data + index * (v)->baseTypeSize;
+        memcpy(destination, source, (v)->baseTypeSize);
+        return;
+    }
+
+    fprintf(stderr, "indexError: index[%u] is not exist", index);
+    exit(1);
+}
+
+void pushBackV(vectorVoid *v, void *source) {
+    if (isFullV(v))
+        reserveV(v, (v)->capacity * 2);
+    else if((v)->capacity == 0)
+        reserveV(v, 1);
+
+    char *lastElement = (char *) (v)->data + (v)->size * (v)->baseTypeSize;
+    memcpy(lastElement, source, (v)->baseTypeSize);
+}
+
+void popBackV(vectorVoid *v){
+    if((v)->size == 0){
+        fprintf(stderr, "Is empty vector!");
+        exit(1);
+    }
+
+    (v)->size--;
 }
 
 
