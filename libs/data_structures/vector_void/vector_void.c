3bcdc1f82bd2f352aca841d505d7a3e1af0f9f91
blyatifull <ilya.lebedev.2003@list.ru>
2022-02-08 19:19:45 +0300

    memory usage of vectorVoid

--- libs/data_structures/vector_void/vector_void.c
+++ libs/data_structures/vector_void/vector_void.c
@@ -1,1 +1,43 @@
 #include "vector_void.h"
+
+vectorVoid createVectorV(size_t size, size_t baseTypeSize){
+    int *v = (void *)malloc(size * baseTypeSize);
+
+    if (v == NULL) {
+        fprintf(stderr, "bad alloc");
+        exit(1);
+    }
+
+    return (vectorVoid) {v, 0, size, baseTypeSize};
+}
+
+void reserveV(vectorVoid *v, size_t newCapacity){
+    if (newCapacity == 0) {
+        (v)->data = NULL;
+    }
+    else if (newCapacity < (v)->size) {
+        (v)->size = newCapacity;
+    }
+    else if ((v)->data == NULL) {
+        fprintf(stderr, "bad alloc");
+        exit(1);
+    }
+
+    (v)->data = (void *)realloc((v)->data, newCapacity * v->baseTypeSize);
+    (v)->capacity = newCapacity;
+}
+
+void shrinkToFitV(vectorVoid *v){
+    reserveV(v, 0);
+}
+
+void clearV(vectorVoid *v){
+    v->size = 0;
+}
+
+void deleteVector(vectorVoid  *v){
+    free(v->data);
+}
+
+
+
--- libs/data_structures/vector_void/vector_void.h
+++ libs/data_structures/vector_void/vector_void.h
@@ -18,6 +18,15 @@ typedef struct vectorVoid{
     size_t baseTypeSize;        // размер базового типа
 }vectorVoid;
 
+vectorVoid createVectorV(size_t n, size_t baseTypeSize);
+
+void reserveV(vectorVoid *v, size_t newCapacity);
+
+void shrinkToFitV(vectorVoid *v);
+
+void clearV(vectorVoid *v);
+
+void deleteVectorV(vectorVoid *v);
 
 
 #endif //COURSE_VECTOR_VOID_H
